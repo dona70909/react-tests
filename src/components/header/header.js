af802919b7df6368152d23a411a1adb365ec29cf
Donatella7 <campaniellodonatella@gmail.com>
2022-07-13 19:38:29 +0200

    bootstrap

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,7 +10,7 @@ class Header extends React.Component {
     render() {
         return (
     
-            <h1>Header</h1>
+            <h1 className='text-danger'>Header</h1>
 
         )
     }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Main from './components/main/Main';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
