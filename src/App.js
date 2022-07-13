4b662936a976f802c92764202c9e276fcb452304
Donatella7 <campaniellodonatella@gmail.com>
2022-07-13 23:08:23 +0200

    skeleton sidebar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 
 import React from 'react';
 
-import './App.css';
+/* //# css */
+import './css/App.css'; 
+
+/* //# components */
 import Main from './components/main/Main';
 import Header from './components/header/Header';
 import SideBar from './components/header/SideBar';
@@ -18,13 +21,13 @@ class App extends React.Component {
 
     return (
 
-      <section className="App d-flex p-2">
+      <section className="App wrapper-app d-flex">
         
-        <div className='left-side my-sidebar-wrapper col-2'>
+        <div className='left-side my-sidebar-wrapper col-1 p-2'>
           <SideBar/>
         </div>
 
-        <div className='right-side my-main-content-wrapper col-10'>
+        <div className='right-side my-main-content-wrapper col-11 p-2'>
           <Header/>
           <Main/>
         </div>
--- src/components/header/SideBar.js
+++ src/components/header/SideBar.js
@@ -1,6 +1,9 @@
 
 import React from 'react';
 
+import './css/SideBar.css';
+
+
 /**
  * Questo è il componente sidebar
  */
@@ -9,8 +12,41 @@ class SideBar extends React.Component {
 
     render() {
         return (
+
+            <nav className='d-flex flex-column justify-space-between align-items-center'>
+
+
+                <h2 className='mb-5 text-center'> 
+                    <i class="bi bi-pie-chart" id='bi-pie-chart'></i>
+                </h2>
     
-            <h1 className='text-danger'>Side</h1>
+                <ul className='list-unstyled d-flex flex-column align-items-center'>
+
+                    <li>
+                        <i class="bi bi-clock" id='my-clock-icon'></i>
+                    </li>
+
+                    <li>
+                        <i class="bi bi-chart"></i>
+                    </li>
+
+                    <li>
+                        <i class="bi bi-journal-text"></i>
+                    </li>
+
+                    <li>
+                        <i class="bi bi-gear-wide-connected"></i>
+                    </li>
+
+                    <li>
+                        <i class="bi bi-calendar4-week"></i>
+                    </li>
+
+                    <li>
+                        <i class="bi bi-gear-fill"></i>
+                    </li>
+                </ul>
+            </nav>
 
         )
     }
